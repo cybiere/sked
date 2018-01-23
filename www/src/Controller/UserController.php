e76612e83d95060938b2b32721361bd2b6a1f8b9
Nicolas Cosnard <nicolas@cosnard.io>
2018-01-23 21:28:11 +0100

    added template management

--- www/src/Controller/UserController.php
+++ www/src/Controller/UserController.php
@@ -3,16 +3,17 @@ namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 
 /**
  * @Route("/user")
  */
-class UserController {
+class UserController extends Controller{
 	/**
 	 * @Route("/")
 	 */
 	public function index(){
-		return new Response("<html><body>OK</body></html>");
+		return $this->render('user/index.html.twig',array('userList'=>array('titi','toto')));
 	}
 }
