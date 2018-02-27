d4dc1bafd6938089d23ef642de57e6c7c0463d62
Nicolas Cosnard <nicolas@cosnard.io>
2018-02-27 22:20:55 +0100

    cant enroled again a user which is already enroled

--- www/src/Controller/UserController.php
+++ www/src/Controller/UserController.php
@@ -29,32 +29,36 @@ class UserController extends Controller{
 
 		if ($form->isSubmitted() && $form->isValid()) {
 			$user = $form->getData();
+			$userRepository = $this->getDoctrine()->getRepository(User::class);
 
-			//check the user is an existing LDAP user
-			$ldap = Ldap::create('ext_ldap', array('connection_string' => 'ldap://'.$this->container->getParameter('ldap_url').':'.$this->container->getParameter('ldap_port')));
-			$ldap->bind($this->container->getParameter('ldap_bind_dn'), $this->container->getParameter('ldap_bind_pw'));
-			$ldapQuery = $ldap->query($this->container->getParameter('ldap_base_dn'), '(&(objectclass=person)(uid='.$user->getUsername().'))');
-			$ldapResults = $ldapQuery->execute()->toArray();
-
-			if(isset($ldapResults[0]) && $ldapResults[0]->getAttribute('uid')[0] == $user->getUsername()){
-				$user->setFullname($ldapResults[0]->getAttribute('cn')[0]);
-				$user->setEmail($ldapResults[0]->getAttribute('mail')[0]);
-
-				$em->persist($user);
-				$em->flush();
-				echo "<script>alert('user ".$user->getFullname()." found')</script>";
-				$user = new User();
-				$form = $this->createFormBuilder($user)
-					->add('username', TextType::class)
-					->add('save', SubmitType::class, array('label' => 'Create User'))
-					->getForm();
+			//check the user is not already existing
+			if($userRepository->findOneBy(['username' => $user->getUsername()])){
+				echo "<script>alert('user already enroled')</script>";
 			}else{
-				echo "<script>alert('user not found')</script>";
+				//check the user is an existing LDAP user
+				$ldap = Ldap::create('ext_ldap', array('connection_string' => 'ldap://'.$this->container->getParameter('ldap_url').':'.$this->container->getParameter('ldap_port')));
+				$ldap->bind($this->container->getParameter('ldap_bind_dn'), $this->container->getParameter('ldap_bind_pw'));
+				$ldapQuery = $ldap->query($this->container->getParameter('ldap_base_dn'), '(&(objectclass=person)(uid='.$user->getUsername().'))');
+				$ldapResults = $ldapQuery->execute()->toArray();
+
+				if(isset($ldapResults[0]) && $ldapResults[0]->getAttribute('uid')[0] == $user->getUsername()){
+					$user->setFullname($ldapResults[0]->getAttribute('cn')[0]);
+					$user->setEmail($ldapResults[0]->getAttribute('mail')[0]);
+
+					$em->persist($user);
+					$em->flush();
+					echo "<script>alert('user ".$user->getFullname()." found')</script>";
+					$user = new User();
+					$form = $this->createFormBuilder($user)
+						->add('username', TextType::class)
+						->add('save', SubmitType::class, array('label' => 'Create User'))
+						->getForm();
+				}else{
+					echo "<script>alert('user not found')</script>";
+				}
 			}
 		}
 
-
-		$userRepository = $this->getDoctrine()->getRepository(User::class);
 		$users = $userRepository->findAll();
 
 		return $this->render('user/index.html.twig',array('users'=>$users,'form'=>$form->createView()));
