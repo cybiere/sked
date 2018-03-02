db32a95306199e1ca4344512283cf2f901c7fe35
Nicolas Cosnard <nicolas@cosnard.io>
2018-03-02 20:09:54 +0100

    Added project entity base attributes

--- www/src/Entity/Project.php
+++ www/src/Entity/Project.php
@@ -16,5 +16,85 @@ class Project
      */
     private $id;
 
-    // add your own fields
+	/**
+     * @ORM\Column(type="string", length=10)
+     */
+	private $reference;
+
+	/**
+     * @ORM\Column(type="string", length=200)
+     */
+	private $name;
+
+	/**
+     * @ORM\Column(type="string", length=200)
+     */
+	private $client;
+
+	/**
+     * @ORM\Column(type="integer")
+     */
+	private $status;
+
+	/**
+     * @ORM\Column(type="integer")
+     */
+	private $nbDays;
+
+	/**
+     * @ORM\Column(type="string", length=1000)
+     */
+	private $comments;
+
+	public function getId(){
+		return $this->id;
+	}
+
+	public function getReference(){
+		return $this->reference;
+	}
+
+	public function setReference($reference){
+		$this->reference = $reference;
+	}
+
+	public function getName(){
+		return $this->name;
+	}
+
+	public function setName($name){
+		$this->name = $name;
+	}
+
+	public function getClient(){
+		return $this->client;
+	}
+
+	public function setClient($client){
+		$this->client = $client;
+	}
+
+	public function getStatus(){
+		return $this->status;
+	}
+
+	public function setStatus($status){
+		$this->status = $status;
+	}
+
+	public function getNbDays(){
+		return $this->nbDays;
+	}
+
+	public function setNbDays($nbDays){
+		$this->nbDays = $nbDays;
+	}
+
+	public function getComments(){
+		return $this->comments;
+	}
+
+	public function setComments($comments){
+		$this->comments = $comments;
+	}
 }
