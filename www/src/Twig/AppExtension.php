3489c4109baaf71c8b11e302f0a906036fb3acc5
Nicolas Cosnard <nicolas@cosnard.io>
2018-03-03 17:41:35 +0100

    Styling for kanban

--- www/src/Twig/AppExtension.php
+++ www/src/Twig/AppExtension.php
@@ -11,37 +11,50 @@ class AppExtension extends AbstractExtension
 {
 	private $router;
 
-    public function __construct(UrlGeneratorInterface $router)
-    {
-        $this->router = $router;
+	public function __construct(UrlGeneratorInterface $router)
+	{
+		$this->router = $router;
 	}
 
-    public function getFunctions()
-    {
+	public function getFunctions()
+	{
 		return array(
 			new TwigFunction('kanproject',array($this,'kanprojectFunction')),
-        );
-    }
+		);
+	}
 
 	public function kanprojectFunction($project){
-		echo "<div class='kanProject'>\n";
-		echo "<h4>".$project->getName()." (".$project->getReference().")</h4>\n";
-		echo "<p class='kanActions'>\n";
-			echo "<a href='".$this->router->generate('project_edit',array("projectId"=>$project->getId()))."'><i title='Modifier' class='fas fa-edit'></i></a>\n";
-			echo "<a href='".$this->router->generate('project_archive',array("projectId"=>$project->getId()))."'><i title='Archiver' class='fas fa-caret-square-down'></i></a>\n";
-		echo "</p>\n";
-		if($project->getStatus() != 7){
-		echo "<p class='kanMove'>\n";
-		if($project->getStatus() == 0)
-			echo "&lt;\n";
-		else
-			echo "<a href='".$this->router->generate('project_index',array("projectId"=>$project->getId(),"way"=>"dec"))."'>&lt;</a>\n";
-		if($project->getStatus() == 6)
-			echo "&gt;\n";
-		else
-			echo "<a href='".$this->router->generate('project_index',array("projectId"=>$project->getId(),"way"=>"inc"))."'>&gt;</a>\n";
-		echo "</p>\n";
-		}
-		echo "</div>\n";
+?>
+		<div class='card kanProject'>
+		<h4 class="card-header"><?php echo $project->getReference(); ?></h4>
+		<div class="card-body">
+			<ul class="list-unstyled">
+<li> Nom : <?php echo $project->getName(); ?></li>
+<li> Client : <?php echo $project->getClient(); ?></li>
+<li> Jours : <?php echo $project->getNbDays(); ?></li>
+</ul>
+</div>
+<div class="card-footer">
+	<div class="row">
+	<div class="col">
+		<?php if($project->getStatus() == 0) echo "<i class='fas fa-chevron-circle-left'></i>"; else { ?>
+			<a href='<?php echo $this->router->generate('project_index',array("projectId"=>$project->getId(),"way"=>"dec")); ?>'><i class='fas fa-chevron-circle-left'></i></a>
+		<?php } ?>
+	</div>
+	<div class="col">
+			<a href='<?php echo $this->router->generate('project_edit',array("projectId"=>$project->getId())); ?>'><i title='Modifier' class='fas fa-edit'></i></a>
+	</div>
+	<div class="col">
+			<a href='<?php echo $this->router->generate('project_archive',array("projectId"=>$project->getId())); ?>'><i title='Archiver' class='fas fa-caret-square-down'></i></a>
+	</div>
+	<div class="col">
+		<?php if($project->getStatus() == 6) echo "<i class='fas fa-chevron-circle-right'></i>"; else { ?>
+			<a href='<?php echo $this->router->generate('project_index',array("projectId"=>$project->getId(),"way"=>"inc")); ?>'><i class='fas fa-chevron-circle-right'></i></a>
+		<?php } ?>
+		</div>
+		</div>
+		</div>
+		</div>
+<?php
 	}
 }
