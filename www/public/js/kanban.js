fb7bcf83669ce1f8240e3ae24fabd3c170ffbdc0
Nicolas Cosnard <nicolas@cosnard.io>
2018-03-04 10:31:46 +0100

    Draggable progressing, need droppable

--- www/public/js/kanban.js
+++ www/public/js/kanban.js
@@ -1,17 +1,29 @@
-var isDown=false;
-
-$(document).on('mousedown','.kanProject', function(){
-	var isDown=true;
-	console.log('down');
-	$(this).addClass("border-primary");
-});
-
-$(document).on('mouseenter','.kanCol', function(){
-	console.log(isDown);
-	if(isDown){
-		$(this).find('.kanGhost').show();
-	}
-});
-$(document).on('mouseleave','.kanCol', function(){
-	$(this).find('.kanGhost').hide();
+$(function(){
+	$('.kanProject').draggable({
+		containment : '#kanban',
+		revert : true,
+		classes: {
+			"ui-draggable": "highlight"
+		},
+		stack: "div",
+		start : function(){
+			console.log('dragged');
+			$(this).addClass("border-primary");
+			$(this).addClass("kanFloat");
+			$(document).on('mouseenter','.kanCol', function(event){
+				$(this).find('.kanGhost').show();
+			});
+			$(document).on('mouseleave','.kanCol', function(event){
+				$(this).find('.kanGhost').hide();
+			});
+		},
+		stop : function(){
+			$(this).removeClass("border-primary");
+			$(this).removeClass("kanFloat");
+			console.log('released');
+			$(document).off('mouseenter','.kanCol');
+			$(document).off('mouseleave','.kanCol');
+			$('.kanGhost').hide();
+		}
+	}); // appel du plugin
 });
