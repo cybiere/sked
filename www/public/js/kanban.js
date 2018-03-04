e943427a8c2afb6186483fd09f4d0b23f059b574
Nicolas Cosnard <nicolas@cosnard.io>
2018-03-04 15:29:59 +0100

    Kanban works visual (need to save modif in db)

--- www/public/js/kanban.js
+++ www/public/js/kanban.js
@@ -2,28 +2,23 @@ $(function(){
 	$('.kanProject').draggable({
 		containment : '#kanban',
 		revert : true,
-		classes: {
-			"ui-draggable": "highlight"
-		},
-		stack: "div",
+	    revertDuration: 0,
+		cursor: "grab",
 		start : function(){
-			console.log('dragged');
 			$(this).addClass("border-primary");
 			$(this).addClass("kanFloat");
-			$(document).on('mouseenter','.kanCol', function(event){
-				$(this).find('.kanGhost').show();
-			});
-			$(document).on('mouseleave','.kanCol', function(event){
-				$(this).find('.kanGhost').hide();
-			});
 		},
 		stop : function(){
 			$(this).removeClass("border-primary");
 			$(this).removeClass("kanFloat");
-			console.log('released');
-			$(document).off('mouseenter','.kanCol');
-			$(document).off('mouseleave','.kanCol');
-			$('.kanGhost').hide();
 		}
 	}); // appel du plugin
+	$('.kanCol').droppable({
+		classes: {
+			"ui-droppable-hover":"kanActiveCol",
+		},
+		drop : function(event, ui){
+			ui.draggable.detach().appendTo(this);
+		}
+	});
 });
