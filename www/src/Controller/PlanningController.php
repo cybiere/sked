a615d9f6ccb7e6c405c286cea62ef18651dc6eea
Nicolas Cosnard <nicolas@cosnard.io>
2018-03-05 21:57:17 +0100

    Basic planning table view

--- www/src/Controller/PlanningController.php
+++ www/src/Controller/PlanningController.php
@@ -6,6 +6,7 @@ use App\Entity\User;
 use App\Entity\Planning;
 use App\Form\PlanningType;
 
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
@@ -14,13 +15,21 @@ class PlanningController extends Controller
     /**
      * @Route("/", name="planning_index")
      */
-    public function index()
+    public function index(Request $request)
 	{
 		$em = $this->getDoctrine()->getManager();
 		$userRepository = $this->getDoctrine()->getRepository(User::class);
 
 		$planning = new Planning();
 		$form = $this->createForm(PlanningType::class,$planning);
+
+		$form->handleRequest($request);
+		if ($form->isSubmitted() && $form->isValid()) {
+			$planning = $form->getData();
+			$em->persist($planning);
+			$em->flush();
+		 	$this->addFlash('success','Planning ajouté');
+		}
 
 
 		$users = $userRepository->findAll();
