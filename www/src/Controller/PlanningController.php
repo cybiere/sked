5499c63b03a7d9d81e0fc19fe3ee9fa01d6d2ea7
Nicolas Cosnard <nicolas@cosnard.io>
2018-04-01 20:39:47 +0200

    implemented resize banckend

--- www/src/Controller/PlanningController.php
+++ www/src/Controller/PlanningController.php
@@ -7,6 +7,7 @@ use App\Entity\Planning;
 use App\Form\PlanningType;
 
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
@@ -38,5 +39,42 @@ class PlanningController extends Controller
 			'users' => $users,
 			'form' => $form->createView(),
         ]);
-    }
+	}
+
+	/**
+	 * @Route("/resize/{planningId}/{newSize}",name="planning_resize")
+	 */
+	public function resize(Request $request,$planningId,$newSize){
+		$em = $this->getDoctrine()->getManager();
+		$planningRepository = $this->getDoctrine()->getRepository(Planning::class);
+
+		if(!($planning = $planningRepository->find($planningId))){
+			$arrData = ['success' => false, 'errormsg' => 'Elément de planning non trouvé'];
+		}else{
+			if($newSize < 1) $newSize = 1;
+			$planning->setNbSlices($newSize);
+			$em->flush();
+			$arrData = ['success' => true];
+		}
+        return new JsonResponse($arrData);
+	}
+
+	/**
+	 * @Route("/move/{planningId}/{newStatus}",name="planning_move")
+	 */
+	public function move(Request $request,$planningId,$newStatus){
+		$em = $this->getDoctrine()->getManager();
+		$planningRepository = $this->getDoctrine()->getRepository(Planning::class);
+
+		if(!($planning = $planningRepository->find($planningId))){
+			$arrData = ['success' => false, 'errormsg' => 'Projet non trouvé'];
+		}else{
+			if($newStatus < 0) $newStatus = 0;
+			if($newStatus > 7) $newStatus = 7;
+			$planning->setStatus($newStatus);
+			$em->flush();
+			$arrData = ['success' => true];
+		}
+        return new JsonResponse($arrData);
+	}
 }
