a5d253297919790c62bd03a82bfd8572a91e4f63
Nicolas Cosnard <nicolas@cosnard.io>
2018-11-17 19:50:25 +0100

    Move holiday calculation to common class. Fixes #11

--- www/src/Controller/UserController.php
+++ www/src/Controller/UserController.php
@@ -213,32 +213,11 @@ class UserController extends Controller{
 			array('startDate'=>'ASC','startHour'=>'ASC'));
 
 		$startDateObj = new \DateTime();
-		$baseYear = intval($startDateObj->format('Y'));
-		$holidays = array();
-		for($i=-1;$i<=1;$i++){
-			$year=$baseYear+$i;
-			$easterDate  = \easter_date($year);
-			$easterDay   = date('j', $easterDate);
-			$easterMonth = date('n', $easterDate);
-			$easterYear  = date('Y', $easterDate);
 
-			// Dates fixes
-			$holidays[] = mktime(0, 0, 0, 1,  1,  $year);  // 1er janvier
-			$holidays[] = mktime(0, 0, 0, 5,  1,  $year);  // Fête du travail
-			$holidays[] = mktime(0, 0, 0, 5,  8,  $year);  // Victoire des alliés
-			$holidays[] = mktime(0, 0, 0, 7,  14, $year);  // Fête nationale
-			$holidays[] = mktime(0, 0, 0, 8,  15, $year);  // Assomption
-			$holidays[] = mktime(0, 0, 0, 11, 1,  $year);  // Toussaint
-			$holidays[] = mktime(0, 0, 0, 11, 11, $year);  // Armistice
-			$holidays[] = mktime(0, 0, 0, 12, 25, $year);  // Noel
-
-			// Dates variables
-			$holidays[] = mktime(0, 0, 0, $easterMonth, $easterDay + 1,  $easterYear);
-			$holidays[] = mktime(0, 0, 0, $easterMonth, $easterDay + 39, $easterYear);
-			$holidays[] = mktime(0, 0, 0, $easterMonth, $easterDay + 50, $easterYear);
-		}
-		sort($holidays);
-
-		return $this->render('user/view.html.twig',array('user'=>$user,'plannings'=>$plannings,'holidays'=>$holidays));
+		return $this->render('user/view.html.twig',array(
+			'user'=>$user,
+			'plannings'=>$plannings,
+			'holidays'=>Common::getHolidays($startDateObj->format('Y'))
+		));
 	}
 }
